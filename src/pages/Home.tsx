eedb1da1808d95722cc9a575561c64478340b483
malinkacat <gumnickajaliza@gmail.com>
2024-11-01 13:47:08 +0200

    Change sign up and login forms style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,65 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import ellipsLg from "../assets/images/ellipse-lg.png";
+import ellipsMd from "../assets/images/ellipse-md.png";
+import ellipsSm from "../assets/images/ellipse-sm.png";
+import ellipsXs from "../assets/images/ellipse-xs.png";
+
+import Button from "../components/ui/Button";
 
 export default function Home() {
   const navigate = useNavigate();
+
   return (
-    <div className="flex justify-center items-center w-full flex-col gap-10">
-      <h1 className="text-4xl">Home page</h1>
+    <div className="px-[var(--sm-px) md:px-[var(--md-px)] lg:px-[var(--lg-px)]">
+      <div className="flex w-full mt-[calc(var(--navbar-height)+40px)] h-[calc(100vh-var(--navbar-height)-80px)] mb-[40px]">
+        <div className="relative flex flex-col w-full gap-4 justify-center items-center rounded-3xl border-2 border-[var(--border-clr)]">
+          <img
+            className="animate-orbit transition-transform opacity-[80%]
+            absolute top-[-5%] left-[-25%] xl:left-[-15%] w-auto h-[40%] xl:h-[65%]"
+            src={ellipsLg}
+            alt="Ellips Large"
+          />
+          <img
+            className="animate-orbit-reverse transition-transform 
+            absolute bottom-[-5%] right-[-20%] xl:right-[-10%] w-auto"
+            src={ellipsMd}
+            alt="Ellips Medium"
+          />
+          <img
+            className="animate-orbit-reverse transition-transform 
+            absolute bottom-[40%] right-[-20%] xl:right-[-15%] w-auto"
+            src={ellipsSm}
+            alt="Ellips Small"
+          />
+          <img
+            className="animate-orbit transition-transform 
+            absolute bottom-[35%] left-[-10%] xl:bottom-[15%] w-auto"
+            src={ellipsXs}
+            alt="Ellips Xs"
+          />
+          <h1 className="text-[50px] lg:text-[65px] uppercase font-bold">
+            Вчіться ефективніше <br /> за допомогою ШІ
+          </h1>
+          <p className="w-[70%] xl:w-[35%] text-[var(--input-text-clr)]">
+            Завантажте свої роботи, отримайте доступ до навчальних матеріалів та
+            дозвольте ШІ допомагати Вам навчатися
+          </p>
+          <Button params={{ content: "Почати роботу 👾", className: "mt-6" }} />
+        </div>
+      </div>
+      <div className="w-full flex flex-col items-start">
+        <h2 className="text-[32px] uppercase font-bold">
+          Чим корисна платформа LearnIT?
+        </h2>
+        <p className="text-left w-[70%]">
+          Learn.it — це платформа, яка розроблена спеціально для студентів.
+          Завдяки можливостям штучного інтелекту ми допомагаємо студентам
+          завантажувати і перевіряти свої завдання, отримувати доступ до
+          навчальних матеріалів та ефективніше співпрацювати з викладачами
+        </p>
+      </div>
     </div>
   );
 }
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,32 +1,18 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useState, useEffect, useRef, FormEvent } from "react";
+import { useState, useEffect, FormEvent } from "react";
 import axios from "axios";
 
-import LabeledInput from "../components/ui/LabeledInput";
-import LabeledSelect from "../components/ui/LabeledSelect";
-import Button from "../components/ui/Button";
-
-import hiddenPassword from "../assets/icons/hidden-password.svg";
-import shownPassword from "../assets/icons/shown-password.svg";
+import SignUpForm from "../components/ui/SignUpForm";
 
 import { SignUpUserDataTypes } from "../interfaces/signupContentType";
 
-import { changePasswordVisibility } from "../methods/changePasswordVisibility";
 import { inputsValidation } from "../methods/inputsValidation";
 import { validateData } from "../methods/inputsValidation";
-
-import { cities } from "../lib/cities";
-import { roles } from "../lib/roles";
 
 export default function Signup() {
   const navigate = useNavigate();
 
-  const passwordInputRef = useRef<HTMLInputElement>(null);
-  const confirmPasswordInputRef = useRef<HTMLInputElement>(null);
-
   const url = "http://localhost:8080/";
-
-  const [passwordVisibility, setPasswordVisibility] = useState<Boolean>(false);
 
   const [inputsData, setInputsData] = useState<SignUpUserDataTypes>({
     lastName: "",
@@ -40,7 +26,7 @@ export default function Signup() {
     confirmPassword: "",
   });
 
-  const handleChange = (
+  const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
     const { name, value } = e.target;
@@ -81,190 +67,33 @@ export default function Signup() {
   };
 
   return (
-    <div className="flex flex-col gap-10 justify-center items-center w-full h-screen">
-      <h1 className="text-4xl">Реєстрація</h1>
-      <form className="sm:w-[60%] md:w-[40%] lg:w-[20%]">
-        <LabeledInput
-          labelParams={{
-            content: "Прізвище",
-          }}
-          inputParams={{
-            id: "last-name",
-            value: inputsData.lastName,
-            type: "text",
-            inputName: "lastName",
-            functionName: handleChange,
-            isRequired: true,
-          }}
-        />
-        <LabeledInput
-          labelParams={{
-            content: "Ім'я",
-          }}
-          inputParams={{
-            id: "first-name",
-            value: inputsData.firstName,
-            type: "text",
-            inputName: "firstName",
-            functionName: handleChange,
-            isRequired: true,
-          }}
-        />
-        <LabeledInput
-          labelParams={{
-            content: "По батькові",
-          }}
-          inputParams={{
-            id: "father-name",
-            value: inputsData.fatherName,
-            type: "text",
-            inputName: "fatherName",
-            functionName: handleChange,
-            isRequired: true,
-          }}
-        />
-
-        <div className="flex flex-row gap-5">
-          <LabeledSelect
-            labelParams={{
-              content: "Оберіть місто",
-            }}
-            selectParams={{
-              id: "cities",
-              value: inputsData.city,
-              selectName: "city",
-              functionName: handleChange,
-              isRequired: true,
-              optionsContent: cities,
-            }}
-          />
-          <LabeledSelect
-            labelParams={{
-              content: "Тип профілю",
-            }}
-            selectParams={{
-              id: "roles",
-              value: inputsData.profileType,
-              selectName: "profileType",
-              functionName: handleChange,
-              isRequired: true,
-              optionsContent: roles,
-            }}
-          />
-        </div>
-        <div className="relative">
-          <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pt-6 pointer-events-none">
-            <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 16"
-            >
-              <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
-              <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
-            </svg>
-          </div>
-          <LabeledInput
-            labelParams={{
-              content: "Електронна пошта",
-            }}
-            inputParams={{
-              id: "email-address",
-              value: inputsData.email,
-              type: "text",
-              inputName: "email",
-              functionName: handleChange,
-              placeholderText: "address@email.com",
-              inputClassName: "pl-10",
-              isRequired: true,
-            }}
-          />
-        </div>
-        <div className="relative">
-          <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pt-6 pointer-events-none">
-            <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 16"
-            >
-              <path d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
-            </svg>
-          </div>
-          <LabeledInput
-            labelParams={{
-              content: "Номер телефону",
-            }}
-            inputParams={{
-              id: "phone-number",
-              value: inputsData.phoneNumber,
-              type: "text",
-              inputName: "phoneNumber",
-              functionName: handleChange,
-              placeholderText: "+38-ХХХ-ХХХ-ХХХХ",
-              inputClassName: "pl-10",
-              isRequired: true,
-              length: 16,
-            }}
-          />
-        </div>
-
-        <div className="flex flex-row gap-5 items-center">
-          <LabeledInput
-            labelParams={{
-              content: "Пароль",
-            }}
-            inputParams={{
-              id: "password",
-              value: inputsData.password,
-              type: "password",
-              inputName: "password",
-              functionName: handleChange,
-              isRequired: true,
-              ref: passwordInputRef,
-            }}
-          />
-          <LabeledInput
-            labelParams={{
-              content: "Підтвердіть пароль",
-            }}
-            inputParams={{
-              id: "confirm-password",
-              value: inputsData.confirmPassword,
-              type: "password",
-              inputName: "confirmPassword",
-              functionName: handleChange,
-              isRequired: true,
-              ref: confirmPasswordInputRef,
-            }}
-          />
-          <img
-            src={passwordVisibility ? shownPassword : hiddenPassword}
-            onClick={() =>
-              changePasswordVisibility(
-                [passwordInputRef, confirmPasswordInputRef],
-                [passwordVisibility, setPasswordVisibility]
-              )
-            }
-            alt="Show/hide password"
-            className="cursor-pointer h-5 w-auto"
-          />
-        </div>
-        <Button
+    <div className="flex flex-col md:flex-row h-[calc(100vh-var(--navbar-height))] sm:mb-[var(--navbar-height)] mt-[var(--navbar-height)] justify-center">
+      <div
+        className="flex flex-1 flex-col gap-10 justify-center items-center w-full
+                  px-[var(--sm-px)] md:px-[var(--md-px)] lg:px-[var(--lg-px)] mt-[var(--navbar-height)]"
+      >
+        <h1 className="text-4xl">Реєстрація</h1>
+        <SignUpForm
           params={{
-            content: "Створити профіль",
-            onClickFunction: handleSignUp,
+            inputsData: inputsData,
+            handleSignUp: handleSignUp,
+            handleChange: handleInputChange,
           }}
         />
-      </form>
-      <p>
-        Вже маєте створений профіль?
-        <Link to="/login">
-          <b> Увійти</b>
-        </Link>
-      </p>
+        <p className="text-[var(--input-text-clr)]">
+          Вже маєте створений профіль? <span> </span>
+          <Link to="/login" className="underline">
+            <b>Увійти</b>
+          </Link>
+        </p>
+        <p className="text-[var(--input-text-clr)] mt-6">
+          Реєструючись, ви погоджуєтеся з Умовами використання{" "}
+          <span className="underline">Learn.it</span>
+        </p>
+      </div>
+      <div className="hidden lg:flex flex-1 w-full h-[calc(100vh-80px)]">
+        <div className="w-full h-full bg-[var(--primary-clr)] "></div>
+      </div>
     </div>
   );
 }
