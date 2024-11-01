0384735872f875b98cb2a10f43c046562b2b504d
malinkacat <gumnickajaliza@gmail.com>
2024-11-01 14:21:57 +0200

    Add footer components

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="w-screen h-56 bg-[var(--footer-clr)] mt-32 border-t-2 border-[var(--border-clr)] flex justify-center items-center">
+      <p className="text-3xl font-bold">LearnIT</p>
+    </div>
+  );
+}
